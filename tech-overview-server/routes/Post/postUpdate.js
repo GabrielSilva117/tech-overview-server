853588da716721bec7b6e9cfe9df52c7e21f89ec
Gabriel <gabriel.f.silva117@gmail.com>
2023-02-15 16:07:52 -0300

    feat: Adds a title and category value validation

--- tech-overview-server/routes/Post/postUpdate.js
+++ tech-overview-server/routes/Post/postUpdate.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 require('../../model/Post')
 const Post = mongoose.model('Post')
+require('../../model/Category')
+const Category = mongoose.model('Category')
 const express = require('express')
 const router = express.Router()
 
@@ -13,6 +15,35 @@ router.put('/post/:id', async (req, res) => {
   }
 
   const { title, msg, category } = req.body
+
+  if (title) {
+    const postInfo = await Post.findOne({ title })
+    if (postInfo) {
+      return res.status(403).json({
+        Forbidden: 'This title is already in use'
+      })
+    }
+    if (title.length > 30) {
+      return res.status(403).json({
+        Forbidden:
+          'The title is too long, the maximum length of a title is 30 chars'
+      })
+    }
+  }
+
+  if (category) {
+    if (category.length !== 24) {
+      return res.status(401).json({
+        msg: 'Invalid category Id! Try again.'
+      })
+    }
+    const categoryInfo = await Category.findOne({ _id: category })
+    if (!categoryInfo) {
+      return res.status(404).json({
+        msg: 'The id provided is either invalid or it belongs to a deleted category, try again.'
+      })
+    }
+  }
 
   const post = await Post.findOne({ _id: postId })
   if (!post) {
